85891fea75ce6e66c236ef3c6325fb45831a8a1f
Pelle Wessman <pelle@kodfabrik.se>
2024-04-08 14:19:03 +0200

    Extract logging implementation

--- cli.js
+++ cli.js
@@ -2,12 +2,12 @@
 
 import path from 'node:path';
 
-import chalk from 'chalk';
 import { lilconfig } from 'lilconfig';
 import meow from 'meow';
 import { messageWithCauses, stackWithCauses } from 'pony-cause';
 
 import { addAllIgnores } from './index.js';
+import { log, verboseLog as rawVerboseLog } from './lib/log.js';
 
 // *** CLI setup ***
 
@@ -66,40 +66,8 @@ const {
 
 // *** Logging setup ***
 
-const LOG_TITLE_PAD = 30;
-const LOG_MESSAGE_PAD = 25;
-const LOG_TITLE_PAD_LONG = LOG_TITLE_PAD + LOG_MESSAGE_PAD + 1;
-const LOG_MESSAGE_PAD_LONG = LOG_MESSAGE_PAD + 10;
-
-/**
- * @param {'log'|'error'} level
- * @param {string} title
- * @param {string} [message]
- * @param {string} [extras]
- * @param {boolean} [dimItAll]
- * @returns {void}
- */
-const log = (level, title, message, extras, dimItAll) => {
-  /** @type {string} */
-  let resolvedMessage = [
-    title && chalk.bold((message || extras) ? title.padEnd(title.length > LOG_TITLE_PAD ? LOG_TITLE_PAD_LONG : LOG_TITLE_PAD) : title),
-    extras ? (message || '').padEnd(message && message.length >= LOG_MESSAGE_PAD ? LOG_MESSAGE_PAD_LONG : LOG_MESSAGE_PAD) : message,
-    extras && chalk.dim(extras),
-  ]
-    .filter(item => !!item)
-    .join(' ');
-
-  if (dimItAll) resolvedMessage = chalk.dim(resolvedMessage);
-  if (level === 'error') resolvedMessage = chalk.bgRed.whiteBright(resolvedMessage);
-
-  // eslint-disable-next-line no-console
-  console[level](resolvedMessage);
-};
-
 /** @type {import('./index.js').VerboseLog} */
-const verboseLog = (verbose || debug)
-  ? (title, message, extras, dimItAll) => log('log', title, message, extras, dimItAll)
-  : () => {};
+const verboseLog = (verbose || debug) ? rawVerboseLog : () => {};
 
 verboseLog('Time to ignore TypeScript imports!');
 
